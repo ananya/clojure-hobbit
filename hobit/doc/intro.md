898f025b0f1a8d3e53276831cfb3741b8aabb32a
ananya <ananyaagrawal125@gmail.com>
2020-05-27 03:55:42 +0530

    hobbit code

---
+++ hobit/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hobit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ hobit/test/hobit/core_test.clj
@@ -0,0 +1,7 @@
+(ns hobit.core-test
+  (:require [clojure.test :refer :all]
+            [hobit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
