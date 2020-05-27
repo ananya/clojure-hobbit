72b4fd1d05150e388fe83e2a5b2f87749b2f4b16
ananya <ananyaagrawal125@gmail.com>
2020-05-27 20:42:20 +0530

    ch 3 exercise+ch4 theory

--- hobit/src/hobit/core.clj
+++ hobit/src/hobit/core.clj
@@ -91,3 +91,45 @@
 (defn dec-maker
   [dec-by]
   #(- % dec-by))      ;(* 8 3) == #(* % 3)
+
+;ex 4
+; mapset works like map but return set
+; (mapset inc [1 1 2 2])
+; => #{2 3}
+(defn mapset
+  [func & args]
+  (set (map func (first args)))
+  )
+
+
+
+
+
+;CHAPTER 4
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+(defn even-numbers
+([] (even-numbers 0))
+([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
+
+; (take 10 (even-numbers))
